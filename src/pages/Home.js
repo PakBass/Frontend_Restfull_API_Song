fe9e9af579d26268a77b8bf25e3d7f5439570e12
PakBass <andhibasuki@gmail.com>
2023-09-19 14:48:36 +0800

    menambah fitur detail data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,6 +17,28 @@ function Home() {
   const [currentPage, setCurrentPage] = useState(0); // Menentukan halaman saat ini
   const itemsPerPage = 5; // Jumlah item per halaman
 
+  const handleClick = (song) => {
+    Swal.fire({
+      title: `Nama Band: ${song.nama}`,
+      html: `
+        <p>Judul Lagu: <b>${song.judul_lagu}</p></p>
+        <img 
+            src="${song.image}"
+            style="width: 250px; height: 170px;"
+            alt="Cover Lagu"
+            class="rounded" />
+      `,
+      icon: 'info',
+      showCancelButton: false,
+      focusConfirm: false,
+      confirmButtonText: 'OK',
+    }).then((result) => {
+    if (result.isConfirmed) {
+      window.location.href = '/home'; // Mengarahkan kembali ke halaman home setelah klik OK
+    }
+  });
+};
+
   const fetchSongs = async () => {
     try {
       const response = await axios.get('http://localhost:8000/api/songs');
@@ -117,21 +139,6 @@ function Home() {
     });
   };
 
-  const handleClick = (song) => {
-    Swal.fire({
-      title: `Detail Lagu: ${song.nama}`,
-      html: `
-        <p>Artis: ${song.judul_lagu}</p>
-        <p>Album: ${song.image}</p>
-      `,
-      icon: 'info',
-      showCloseButton: true,
-      showCancelButton: false,
-      focusConfirm: false,
-      confirmButtonText: 'OK',
-    });
-  };
-
   const handlePageClick = (selected) => {
     setCurrentPage(selected);
   };
@@ -218,14 +225,14 @@ function Home() {
                           <td style={{ verticalAlign: "middle" }}>{song.nama}</td>
                           <td style={{ verticalAlign: "middle" }}>{song.judul_lagu}</td>
                           <td style={{ verticalAlign: "middle", textAlign:"center" }}>
-                          {/* <Link to={`/detail/${song.id}`} style={{ textDecoration: 'none' }}>
+                          <Link to={`/detail/${song.id}`} style={{ textDecoration: 'none' }}>
                             <i 
                               className="bi bi-file-earmark-music"
                               data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Tooltip on top"
                               style={{ cursor: "pointer", color: "black", marginRight: "10px" }}
                               onClick={() => handleClick(song)}
                             ></i>
-                          </Link> */}
+                          </Link>
                           <Link to={`/edit/${song.id}`} style={{ textDecoration: 'none' }}>
                           <i 
                             className="bi bi-pencil-square" 
